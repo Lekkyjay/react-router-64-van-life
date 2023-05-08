67e23f683cf61e0c48e4908722190f2cbdecd31a
‘Lekky <lekkyjay@yahoo.com>
2023-05-08 15:22:55 +0200

    Updated HostLayout component.

--- src/components/HostLayout.tsx
+++ src/components/HostLayout.tsx
@@ -1,7 +1,23 @@
-import { Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
 export default function HostLayout() {
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616"
+  }
+
+  const navLinkStyles = ({ isActive }: { isActive: Boolean }) => isActive ? activeStyles : {}
+
   return (
-    <div><Outlet /></div>
+    <>
+      <nav className="host-nav">
+        <NavLink to="." end style={navLinkStyles}>Dashboard</NavLink>
+        <NavLink to="income" style={navLinkStyles}>Income</NavLink>        
+        <NavLink to="vans" style={navLinkStyles}>Vans</NavLink>
+        <NavLink to="reviews" style={navLinkStyles}>Reviews</NavLink>
+      </nav>
+      <Outlet />
+    </>
   )
 }
