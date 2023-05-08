194e83f5fdab5f99c0266ccc4279d07e45c22fe3
‘Lekky <lekkyjay@yahoo.com>
2023-05-08 12:55:12 +0200

    Header component created.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,28 @@
+import { Link, NavLink } from 'react-router-dom'
+
 export default function Header() {
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616"
+  }
+
+  const navLinkStyles = ({ isActive }: { isActive: Boolean }) => isActive ? activeStyles : {}
+
   return (
-    <div>Header</div>
+    <header>
+      <Link className="site-logo" to="/">#VanLife</Link>
+      <nav>
+        <NavLink to="/host" style={navLinkStyles}>
+          Host
+        </NavLink>
+        <NavLink to="/about" style={navLinkStyles}>
+          About
+        </NavLink>
+        <NavLink to="/vans" style={navLinkStyles}>
+          Vans
+        </NavLink>
+      </nav>
+    </header>
   )
 }
