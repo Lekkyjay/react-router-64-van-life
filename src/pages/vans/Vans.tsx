4202c609f5dc778ecac97542cfb76fe29e532ebb
‘Lekky <lekkyjay@yahoo.com>
2023-05-08 18:13:24 +0200

    Added filtering with searchParams.

--- src/pages/vans/Vans.tsx
+++ src/pages/vans/Vans.tsx
@@ -1,18 +1,25 @@
 import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { IVan } from '../../interfaces'
 
 export default function Vans() {
+  const [searchParams, setSearchParams] = useSearchParams()
   const [vans, setVans] = useState<IVan[]>([])
   const apiURL = 'https://us-central1.gcp.data.mongodb-api.com/app/van-life-poqsu/endpoint/vans'
-
+  
   useEffect(() => {
     fetch(apiURL)
       .then(res => res.json())
       .then(data => setVans(data))
   }, [])
 
-  const vanElements = vans?.map(van => (
+  const typeFilter = searchParams.get('type')
+
+  const filteredVans = typeFilter 
+    ? vans.filter(van => van.type === typeFilter)
+    : vans
+
+  const vanElements = filteredVans?.map(van => (
     <div key={van._id} className="van-tile">
       <Link to={`/vans/${van._id}`}>
         <img src={van.imageUrl} />
@@ -28,6 +35,14 @@ export default function Vans() {
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
+
+      <div className="van-list-filter-buttons">
+        <button onClick={() => setSearchParams({type: 'simple'})} className="van-type simple">Simple</button>
+        <button onClick={() => setSearchParams({type: 'luxury'})} className="van-type luxury">Luxury</button>
+        <button onClick={() => setSearchParams({type: 'rugged'})} className="van-type rugged">Rugged</button>
+        <button onClick={() => setSearchParams({})} className="van-type clear-filters">Clear filter</button>      
+      </div>
+
       <div className="van-list">
         {vanElements}
       </div>
