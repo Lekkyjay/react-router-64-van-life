3722bc16de703435ce57484fda6378640cc1a543
‘Lekky <lekkyjay@yahoo.com>
2023-05-10 14:45:12 +0200

    Formatting.

--- src/pages/host/Dashboard.tsx
+++ src/pages/host/Dashboard.tsx
@@ -11,12 +11,12 @@ export default function Dashboard() {
   const renderHostVanElements = (vans: IVan[]) => {
     const hostVanElements = vans.map((van) => (
       <div className="host-van-single" key={van._id}>
-          <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-          <div className="host-van-info">
-              <h3>{van.name}</h3>
-              <p>${van.price}/day</p>
-          </div>
-          <Link to={`vans/${van._id}`}>View</Link>
+        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+        <div className="host-van-info">
+          <h3>{van.name}</h3>
+          <p>${van.price}/day</p>
+        </div>
+        <Link to={`vans/${van._id}`}>View</Link>
       </div>
     ))
 
