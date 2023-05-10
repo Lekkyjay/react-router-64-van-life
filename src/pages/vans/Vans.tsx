499058681d9907ac58db0fb7b051b78a1136bca6
‘Lekky <lekkyjay@yahoo.com>
2023-05-10 02:51:14 +0200

    Added defer to vansLoader.

--- src/pages/vans/Vans.tsx
+++ src/pages/vans/Vans.tsx
@@ -1,54 +1,67 @@
-import { Link, useLoaderData, useNavigation, useSearchParams } from 'react-router-dom'
+import { Await, Link, defer, useLoaderData, useNavigation, useSearchParams } from 'react-router-dom'
 import { IVan } from '../../interfaces'
 import { getVans } from '../../apiService'
 
 export default function Vans() {
   const [searchParams, setSearchParams] = useSearchParams()
-  const vans = useLoaderData() as IVan[]
+  const vansPromise = useLoaderData() as { vans: IVan[] }
   const navigation = useNavigation()
 
   const typeFilter = searchParams.get('type')
-
-  const filteredVans = typeFilter 
-    ? vans.filter(van => van.type === typeFilter)
-    : vans
-
-  const vanElements = filteredVans?.map(van => (
-    <div key={van._id} className="van-tile">
-      <Link to={van._id} state={{ search: `?${searchParams.toString()}`, type: typeFilter }}>
-        <img src={van.imageUrl} />
-        <div className="van-info">
-          <h3>{van.name}</h3>
-          <p>${van.price}<span>/day</span></p>
-        </div>
-        <i className={`van-type ${van.type} selected`}>{van.type}</i>
-      </Link>
-    </div>
-  ))
 
   if (navigation.state === 'loading') return <h1>Loading ...</h1>
 
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
+      
+      <Await resolve={vansPromise.vans}>
+        {(vans: IVan[]) => {
+          const filteredVans = typeFilter 
+          ? vans.filter(van => van.type === typeFilter)
+          : vans
+      
+        const vanElements = filteredVans?.map(van => (
+          <div key={van._id} className="van-tile">
+            <Link to={van._id} state={{ search: `?${searchParams.toString()}`, type: typeFilter }}>
+              <img src={van.imageUrl} />
+              <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}<span>/day</span></p>
+              </div>
+              <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
+          </div>
+        ))
+        return (
+          <>
+            <div className="van-list-filter-buttons">
+              <button onClick={()=>setSearchParams({type: 'simple'})} className={`van-type simple ${typeFilter==="simple" ? "selected" : ""}`}>
+                Simple
+              </button>
+              <button onClick={() => setSearchParams({type: 'luxury'})} className={`van-type simple ${typeFilter==="luxury" ? "selected" : ""}`}>
+                Luxury
+              </button>
+              <button onClick={() => setSearchParams({type: 'rugged'})} className={`van-type simple ${typeFilter==="rugged" ? "selected" : ""}`}>
+                Rugged
+              </button>
+              {typeFilter 
+                ? (<button onClick={() => setSearchParams({})} className="van-type clear-filters">Clear filter</button>) 
+                : null
+              }
+            </div>
 
-      <div className="van-list-filter-buttons">
-        <button onClick={()=>setSearchParams({type: 'simple'})} className={`van-type simple ${typeFilter==="simple" ? "selected" : ""}`}>Simple</button>
-        <button onClick={() => setSearchParams({type: 'luxury'})} className={`van-type simple ${typeFilter==="luxury" ? "selected" : ""}`}>Luxury</button>
-        <button onClick={() => setSearchParams({type: 'rugged'})} className={`van-type simple ${typeFilter==="rugged" ? "selected" : ""}`}>Rugged</button>
-        {typeFilter 
-          ? (<button onClick={() => setSearchParams({})} className="van-type clear-filters">Clear filter</button>) 
-          : null
-        }
-      </div>
-
-      <div className="van-list">
-        {vanElements}
-      </div>
+            <div className="van-list">
+              {vanElements}
+            </div>
+          </>
+        )
+        }}      
+      </Await>      
     </div>
   )
 }
 
 export function vansLoader() {
-  return getVans()
+  return defer({ vans: getVans() })
 }
